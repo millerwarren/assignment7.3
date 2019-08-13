7cae2f55c958d94182f6159ae609186e8939bac6
millerwarren <53824450+millerwarren@users.noreply.github.com>
2019-08-13 10:58:05 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// var count = 1;
+
+// while (count <= 10) {
+//     console.log(count);
+//     count++;
+// }
+// var count = -10;
+
+// while (count <= 19) {
+//     console.log(count);
+//     count++;
+// }
+// var count = 10;
+
+// while (count <= 40) {
+//     if (count % 2 == 0) {
+//         console.log(count)
+//     }
+//     count++;
+// }
+// var count = 10;
+
+// while (count <= 40) {
+//     if (count % 2 != 0) {
+//         console.log(count)
+//     }
+//     count++;
+// }
+var count = 5;
+
+while (count <= 50) {
+    if (count % 5 == 0 && count % 3 == 0) {
+        console.log(count);
+    }
+
+    count++;
+}
